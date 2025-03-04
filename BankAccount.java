fb11548304f893bc10743d0e2139b34221897df4
MichaelCummings9715 <150955366+MichaelCummings9715@users.noreply.github.com>
2025-03-04 10:56:00 -0800

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,42 +1,42 @@
-
 public class BankAccount {
-	String accountHolderName;
+	String accountHolderName; // attributes
 	int accountNumber;
 	double balance;
 	
-	public BankAccount() {
+	public BankAccount() { // constructor
 		accountHolderName = "";
 		balance = 0.0;
 		accountNumber = 0;
 	}
 	
 	
-	public void deposit(double depositAmount) {
+	public void deposit(double depositAmount) { // Method to deposit money into the account
 		
-		balance += depositAmount;
-		
-	}
-	
-	public double getBalance() {
-		
-		return balance;
+		balance += depositAmount; // Adds the deposited amount to the current balance
 		
 	}
 	
-	public void withdraw(double withdrawlAmount) {
+	public double getBalance() { // get method for balance
 		
-		if (withdrawlAmount > balance) {
-			System.out.println("INSUFFICIENT FUNDS!\n");
+		return balance; // Returns the current balance
+		
+	}
+	
+	public void withdraw(double withdrawlAmount) { // Method to withdraw money from the account
+		
+		if (withdrawlAmount > balance) { // error handle for insufficient withdraw
+			System.out.println("INSUFFICIENT FUNDS!\n");  
 		}
 		else {
-			balance -= withdrawlAmount;
+			balance -= withdrawlAmount; // subtracts the amount from the balance
 		}
 		
 	}
 	
+	// setters
 	public void setName(String name) {
 		
-		accountHolderName = name; 
+		accountHolderName = name;  // Assigns prompted name to the account holder
 	}
 	
 	public void setBalance(double accountBalance) {
@@ -44,8 +44,8 @@ public class BankAccount {
 		balance = accountBalance;
 	}
 	
-	public void displayBalance() {
-		System.out.println("Current balance is: $" + getBalance() + "\n");
+	public void displayBalance() { // Method to display the current balance
+		System.out.println("Current balance is: $" + getBalance() + "\n"); // output user balance
 	}
 	
 
